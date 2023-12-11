43a2c08abd2813b1b90ae07dde613ac2b721b077
Fandangoooooo <alejandro.mergelinagarcia@alumnos.esne.es>
2023-12-11 17:24:56 +0100

    n

--- Assets/ChaingColor.cs
+++ Assets/ChaingColor.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 
 public class ChaingColor : MonoBehaviour
 {
-    [SerializeField]
-    private Sprite sprite;
 
     private Color[,] pixelsArray;
+
+    [SerializeField] private Texture2D texture;
 
     // Start is called before the first frame update
     void Start()
     {
-        pixelsArray = new Color[(int)sprite.rect.width, (int)sprite.rect.width];
+        Sprite[] sprites = Resources.LoadAll<Sprite>(texture.name);
+        //sprites[0].rect.width
+        //sprites[0].pivot
+        pixelsArray = new Color[(int)sprites[0].rect.width, (int)sprites[0].rect.height];
+        Debug.Log((int)sprites[0].pivot.x);
+        ////Texture2D texture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+        ////texture.SetPixels(sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, (int)sprite.textureRect.width, (int)sprite.textureRect.height));
 
-        //Texture2D texture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
-        //texture.SetPixels(sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, (int)sprite.textureRect.width, (int)sprite.textureRect.height));
+        ////Color[] pixels = texture.GetPixels();
+        int spriteWidth = (int)sprites[0].rect.width;
 
-        //Color[] pixels = texture.GetPixels();
-        for (int i = 0; i < (int)sprite.rect.width; i++)
+        for (int i = 0; i < spriteWidth; i++)
         {
-            for (int j = 0; j < (int)sprite.rect.height; j++)
+            for (int j = 0; j < (int)sprites[0].rect.height; j++)
             {
 
-                pixelsArray[i,j] = sprite.texture.GetPixel(i+1, j+1);
-                print(sprite.texture.GetPixel(i + 1, j + 1));
+                pixelsArray[i, j] = sprites[0].texture.GetPixel(i + (int)sprites[0].pivot.x, j + (int)sprites[0].pivot.y);
+                print(pixelsArray[i, j]);
 
             }
         }
-       
+
 
     }
 
