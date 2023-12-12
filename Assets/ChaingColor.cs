bfa3c13bf25d819a380b402e78a9c696b394b076
Fandangoooooo <alejandro.mergelinagarcia@alumnos.esne.es>
2023-12-12 21:08:40 +0100

    Update

--- Assets/ChaingColor.cs
+++ Assets/ChaingColor.cs
@@ -7,6 +7,8 @@ public class ChaingColor : MonoBehaviour
 
     [SerializeField]
     private Sprite sprite;
+    [SerializeField]
+    private Sprite palet;
 
     
     private Sprite spriteFinal;
@@ -26,6 +28,37 @@ public class ChaingColor : MonoBehaviour
         //texture.SetPixels(sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, (int)sprite.textureRect.width, (int)sprite.textureRect.height));
 
         //Color[] pixels = texture.GetPixels();
+        ReadPalet();
+        //PrintIntoSprite();
+
+    }
+
+    private void ReadPalet()
+    {
+
+        for (int i = 0; i < (int)sprite.rect.width; i++)
+        {
+            for (int j = 0; j < (int)sprite.rect.height; j++)
+            {
+                Color currentColor;
+                currentColor.r = palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b).r;
+                currentColor.g = palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b).g;
+                currentColor.b = palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b).b;
+                currentColor.a = sprite.texture.GetPixel(i, j).a;
+                //pixelsArray[i, j] = sprite.texture.GetPixel(i, j);
+                spriteFinal.texture.SetPixel(i, j, currentColor /*palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b)*/);
+
+                print(/*"iteracion "+ i + "/" + j + " = " +*/ sprite.texture.GetPixel(i, j));
+
+            }
+        }
+        spriteFinal.texture.Apply();
+        render.sprite = spriteFinal;
+    }
+
+    private void PrintIntoSprite()
+    {
+
         for (int i = 0; i < (int)sprite.rect.width; i++)
         {
             for (int j = 0; j < (int)sprite.rect.height; j++)
@@ -40,13 +73,6 @@ public class ChaingColor : MonoBehaviour
             }
         }
 
-
     }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
