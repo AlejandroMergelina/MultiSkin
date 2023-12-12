82dc9b2321524a55af84b0f5dc9c9ce3940f4481
Fandangoooooo <alejandro.mergelinagarcia@alumnos.esne.es>
2023-12-12 13:12:03 +0100

    update

--- Assets/ChaingColor.cs
+++ Assets/ChaingColor.cs
@@ -5,37 +5,44 @@ using UnityEngine;
 public class ChaingColor : MonoBehaviour
 {
 
-    private Color[,] pixelsArray;
+    [SerializeField]
+    private Sprite sprite;
 
-    [SerializeField] private Texture2D texture;
+    
+    private Sprite spriteFinal;
+
+    [SerializeField]
+    private SpriteRenderer render;
+
+    private Color[,] pixelsArray;
 
     // Start is called before the first frame update
     void Start()
     {
-        Sprite[] sprites = Resources.LoadAll<Sprite>(texture.name);
-        //sprites[0].rect.width
-        //sprites[0].pivot
-        pixelsArray = new Color[(int)sprites[0].rect.width, (int)sprites[0].rect.height];
-        Debug.Log((int)sprites[0].pivot.x);
-        ////Texture2D texture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
-        ////texture.SetPixels(sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, (int)sprite.textureRect.width, (int)sprite.textureRect.height));
+        pixelsArray = new Color[(int)sprite.rect.width, (int)sprite.rect.width];
+        Texture2D texture = Instantiate(render.sprite.texture);
+        spriteFinal = Sprite.Create(texture, sprite.rect, Vector2.one * 0.5f);
+        //Texture2D texture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+        //texture.SetPixels(sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, (int)sprite.textureRect.width, (int)sprite.textureRect.height));
 
-        ////Color[] pixels = texture.GetPixels();
-        int spriteWidth = (int)sprites[0].rect.width;
-
-        for (int i = 0; i < spriteWidth; i++)
+        //Color[] pixels = texture.GetPixels();
+        for (int i = 0; i < (int)sprite.rect.width; i++)
         {
-            for (int j = 0; j < (int)sprites[0].rect.height; j++)
+            for (int j = 0; j < (int)sprite.rect.height; j++)
             {
 
-                pixelsArray[i, j] = sprites[0].texture.GetPixel(i + (int)sprites[0].pivot.x, j + (int)sprites[0].pivot.y);
-                print(pixelsArray[i, j]);
+                pixelsArray[i, j] = sprite.texture.GetPixel(i, j);
+                spriteFinal.texture.SetPixel(i, j, sprite.texture.GetPixel(i, j));
+                spriteFinal.texture.Apply();
+                render.sprite = spriteFinal;
+                print(/*"iteracion "+ i + "/" + j + " = " +*/ spriteFinal.texture.GetPixel(i, j));
 
             }
         }
 
 
     }
+
 
     // Update is called once per frame
     void Update()
