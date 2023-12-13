cce93378a17897d0c10c9d1a387d8f20ac85ecf4
Fandangoooooo <alejandro.mergelinagarcia@alumnos.esne.es>
2023-12-13 16:26:07 +0100

    clean code

--- Assets/ChaingColor.cs
+++ Assets/ChaingColor.cs
@@ -12,27 +12,10 @@ public class ChaingColor : MonoBehaviour
     [SerializeField]
     private Sprite canvas;
 
-    
-    //private Sprite spriteFinal;
 
-    //[SerializeField]
-    //private SpriteRenderer render;
-
-    private Color[,] pixelsArray;
-
-    // Start is called before the first frame update
     void Start()
     {
-        pixelsArray = new Color[(int)sprite.rect.width, (int)sprite.rect.width];
-        Texture2D texture = Instantiate(/*render.sprite.texture*/canvas.texture);
-        //spriteFinal = Sprite.Create(texture, sprite.rect, Vector2.one * 0.5f);
-        //Texture2D texture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
-        //texture.SetPixels(sprite.texture.GetPixels((int)sprite.textureRect.x, (int)sprite.textureRect.y, (int)sprite.textureRect.width, (int)sprite.textureRect.height));
-
-        //Color[] pixels = texture.GetPixels();
         ReadPalet();
-        //PrintIntoSprite();
-
     }
 
     private void ReadPalet()
@@ -47,35 +30,12 @@ public class ChaingColor : MonoBehaviour
                 currentColor.g = palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b).g;
                 currentColor.b = palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b).b;
                 currentColor.a = sprite.texture.GetPixel(i, j).a;
-                //pixelsArray[i, j] = sprite.texture.GetPixel(i, j);
-                canvas.texture.SetPixel(i, j, currentColor /*palet.texture.GetPixel(((Color32)sprite.texture.GetPixel(i, j)).r, ((Color32)sprite.texture.GetPixel(i, j)).b)*/);
-
-                print(/*"iteracion "+ i + "/" + j + " = " +*/ sprite.texture.GetPixel(i, j));
+                canvas.texture.SetPixel(i, j, currentColor);
 
             }
         }
-        //spriteFinal.texture.Apply();
-        //canvas = spriteFinal;
+
         canvas.texture.Apply();
     }
-
-    //private void PrintIntoSprite()
-    //{
-
-    //    for (int i = 0; i < (int)sprite.rect.width; i++)
-    //    {
-    //        for (int j = 0; j < (int)sprite.rect.height; j++)
-    //        {
-
-    //            pixelsArray[i, j] = sprite.texture.GetPixel(i, j);
-    //            spriteFinal.texture.SetPixel(i, j, sprite.texture.GetPixel(i, j));
-    //            spriteFinal.texture.Apply();
-    //            render.sprite = spriteFinal;
-    //            print(/*"iteracion "+ i + "/" + j + " = " +*/ spriteFinal.texture.GetPixel(i, j));
-
-    //        }
-    //    }
-
-    //}
     
 }
